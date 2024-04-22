d854edba1915803be2b6d0c476d272f75fe0a44f
LindaPermata <permatalinda531@gmail.com>
2024-04-22 15:16:00 +0700

    pembuatan prosedur publik input  serta menerima input panjang dan lebar

--- contoh2.cpp
+++ contoh2.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 class bangunDatar{
+    private:
+       float panjang, lebar;
+    public:
+      float luas;
+
+      void input() { //metode input persegi panjang
+
+        cout << "Masukan panjangnya = " ;
+        cin >> panjang;
+        cout << "masukan Lebarnya = " ;
+        cin >> lebar;
     
+      }
+
+
 }
